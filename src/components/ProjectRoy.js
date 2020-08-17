2ed3e713390de2a905f0bf2a76fffd33526dc32b
Hayden Phillips <hayden.colony@gmail.com>
2020-08-17 13:49:52 +1200

    ProjectRoy w/parallax

--- src/components/ProjectRoy.js
+++ src/components/ProjectRoy.js
@@ -12,167 +12,121 @@ import gif2 from "../assets/images/project_roy/knolling_SS_white.jpg";
 import roy_logo from "../assets/images/project_roy/logo_tree.jpg";
 import roy_logo_white from "../assets/images/project_roy/logo_tree_white.png";
 import care_instructions from "../assets/images/project_roy/care_instructions.jpg";
+import LS_navy_background from '../assets/images/project_roy/LS_navy_background.jpg';
 import LSR_1 from "../assets/images/project_roy/LSR_1.jpg";
 import LSR_2 from "../assets/images/project_roy/LSR_2.jpg";
 import LSR_3 from "../assets/images/project_roy/LSR_3.jpg";
 import LSR_4 from "../assets/images/project_roy/LSR_4.jpg";
 import LSR_5 from "../assets/images/project_roy/LSR_5.jpg";
 import LSR_6 from "../assets/images/project_roy/LSR_6.jpg";
-import ParallaxTest from './ParallaxTest';
+// import ParallaxTest from './ParallaxTest';
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons'
+import { useSpring, animated } from 'react-spring'
+import { useInView } from 'react-intersection-observer'
+import Fade from './fade';
 
 
 
 
-class ProjectRoy extends Component {
-    render() {
+const ProjectRoy = () => {
 
-        let parallax;
-        
-        return (
-            <Fragment>
-                <div className='grid-roy'>
+    let parallax;
 
-                    <div className='className="fullscreen_video gcs6 grs6"'>
-                        <div className='wrapper'>
+    const fade = useSpring({
+        from: { opacity: 0 },
+        to: { opacity: 1 },
+        config: { duration: 4000 }
+    });
+
+    const [ref, inView] = useInView({
+        // threshold: 0,
+        // rootMargin: '400px 0px -200px 0px'
+    });
+    
+    const someString = 'Functionality is key with Roy. We strive to design garments that are purposeful and refined.'
+
+    return (
+
+        <Fragment>
+            <Parallax pages={4} ref={ref => parallax = ref} style={{ display: 'flex', maxWidth: '1920px', margin: 'auto' }}>
+
+                <div className='wrapper'>
+                    <ParallaxLayer offset={0} speed={-0.2} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                        <ReactPlayer
+                            volume={0}
+                            className='player'
+                            url={eva_st}
+                            playing={true}
+                            loop={true}
+                            width="100%"
+                            height="auto"
+                            playbackRate={2}
+                        />
+                    </ParallaxLayer>
+
+                    <ParallaxLayer offset={1.9} speed={0.5} style={{ backgroundColor: '#87BCDE' }} />
+                    <ParallaxLayer offset={1} speed={1.2} style={{ backgroundColor: 'white' }} />
+                    <ParallaxLayer offset={3} speed={1} style={{ backgroundImage: `url(${LS_navy_background})`, backgroundSize: 'cover', backgroundPosition: 'center', width: '120%', marginLeft: '-15%' }} />
+
+
+                    <div className='pad'>
+                        <ParallaxLayer offset={0.0} speed={1} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                            <div className='title'>
+                                <h1>ROY APPAREL</h1>
+                            </div>
+                        </ParallaxLayer>
+                        <ParallaxLayer offset={0.1} speed={0.8} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                            <div className='sub-title'>
+                                <p>Designed & Manufactured in New Zealand</p>
+                            </div>
+                        </ParallaxLayer>
+                        <ParallaxLayer offset={0.3} speed={0.3} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                            <img src={roy_logo_white} className='logo-small' />
+                        </ParallaxLayer>
+
+                        <ParallaxLayer offset={1} speed={0.7} style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+                            <img className='image' src={gif1} alt="" />
+                        </ParallaxLayer>
+                        <ParallaxLayer offset={1} speed={1} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                            <img className='image' src={gif2} alt="" />
+                        </ParallaxLayer>
+                        <ParallaxLayer offset={1} speed={0.6} style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+                            <img className='image' src={gif2} alt="" />
+                        </ParallaxLayer>
+
+                        <ParallaxLayer offset={1.7} speed={0.6} style={{ display: 'flex', alignItems: 'flex-end', marginLeft: '5%' }}>
                             <ReactPlayer
-                                className='player'
                                 volume={0}
-                                url={eva_st}
+                                url={swinging_tag}
                                 playing={true}
                                 loop={true}
-                                width="100%"
+                                width="60%"
                                 height="auto"
-                                playbackRate={2}
                             />
+                        </ParallaxLayer>
 
-                            <div className='overlay'>
-                                <Parallax pages={2} ref={ref => parallax = ref}>
-                                    <ParallaxLayer offset={1.3} speed={-0.4}>
-                                        <img src={roy_logo_white} className='logo-small' />
-                                    </ParallaxLayer>
-                                    <ParallaxLayer offset={1.5} speed={0}>
-                                        <div className='content'>
-                                            <div className='title'>
-                                                ROY APPAREL
-                                            </div>
-                                            {/* <div className='description'>
-                                                Designed & Manufactured in New Zealand
-                                            </div> */}
-                                        </div>
-
-                                    </ParallaxLayer>
-                                    <ParallaxLayer offset={0.9} speed={3}>
-                                        <span>I'm fast!</span>
-                                    </ParallaxLayer>
-                                    <ParallaxLayer speed={0.2} offset={1.6}>
-                                        <div className='content'>
-                                            <div className='description'>
-                                                Designed & Manufactured in New Zealand
-                                            </div>
-                                        </div>
-                                    </ParallaxLayer>
-                                </Parallax>
-
-
-                                {/* <ParallaxTest /> */}
-                                {/* <div className='logo-small'> */}
-                                {/* <img src={roy_logo_white} className='logo-small'/> */}
-                                {/* </div> */}
-                                {/* <div className='content'>
-
-
-                                    <div className='title'>
-                                        ROY APPAREL
-                                    </div>
-
-                                    <div className='description'>
-                                        Designed & Manufactured in New Zealand
-                                    </div>
-
-                                </div> */}
+                        <ParallaxLayer offset={1.99} speed={0.8} style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', justifyItems: 'flex-end', marginRight: '5%' }}>
+                            <div className='text-30' ref={ref}>
+                                {inView ? <Fade tagline={someString} /> : <div className='empty' />}
                             </div>
-                        </div>
+                        </ParallaxLayer>
 
+                        <ParallaxLayer offset={1.99} speed={0.8} style={{ display: 'flex', alignItems: 'center' }}>
+                            <img className="care" src={care_instructions} alt="care_instructions" />
+                        </ParallaxLayer>
 
-
-
-                    </div>
-
-                    <div className='gcs6'>
-                        <img id='roy_logo' src={roy_logo} alt="roy_logo" />
-                    </div>
-                    <div className='gcs6'>
-                        <h1>Roy Apparel</h1>
                     </div>
                 </div>
-            </Fragment >
-        )
-    }
+            </Parallax>
+        </Fragment >
+    )
 }
 
 export default ProjectRoy
 
 
 
-{/* <div className="box gcs4">
-<h1>Roy Apparel</h1>
-</div>
-<div className="box gcs2">
-<img src={roy_logo} alt="roy_logo" />
-</div>
-
-<div className="image gcs2">
-<img src={gif1} alt="" />
-</div>
-<div className="image gcs2">
-<img src={gif2} alt="" />
-</div>
-<div className="image gcs2">
-<img src={gif1} alt="" />
-</div>
-
-
-
-<div className="video_clip gcs4">
-<ReactPlayer
-    url={swinging_tag}
-    playing={true}
-    loop={true}
-    width="100%"
-    height="100%"
-    volume={0}
-/>
-</div>
-
-<div className="image_high gcs2">
-<img className="care" src={care_instructions} alt="care_instructions" />
-</div>
-
-
-
-<div className="image_wide gcs6">
-
-</div>
-
-
-
-<div className="box gcs2"></div>
-
-<div className="video_clip gcs4">
-<ReactPlayer
-    volume={0}
-    url={eva_st}
-    playing={true}
-    loop={true}
-    width="100%"
-    height="100%"
-    playbackRate={2}
-/>
-</div>
-
-
-
+/*{
 
 <div className="image_6 gcs6">
 <img src={LSR_1} alt="shirt_1" />
@@ -200,8 +154,6 @@ export default ProjectRoy
 <div className="box gcs2"></div>
 
 
-
-
 <div className="box gcs2"></div>
 
 <div className="video_clip gcs4">
@@ -214,8 +166,6 @@ export default ProjectRoy
     volume={0}
 />
 </div>
-
-
 
 
 <div className="video_clip gcs4">
@@ -231,8 +181,6 @@ export default ProjectRoy
 <div className="box gcs2"></div>
 
 
-
-
 <div className="box gcs2"></div>
 
 <div className="video_clip gcs4">
@@ -246,9 +194,7 @@ export default ProjectRoy
 />
 </div>
 
-
-
-<div className="box gcs2"></div> */}
+<div className="box gcs2"></div> */
 
 //<div id="placeholder" className="video_clip gcs4">
 //{/* <iframe src="https://drive.google.com/file/d/1Er6MLu7Urgyd6swxhnXK_c9j1CYL7pJc/preview" width="100%" height="480"></iframe> */}
---
+++ src/components/fade.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react'
+import { useSpring, animated } from 'react-spring'
+
+
+
+const Fade = (props) => {
+
+  const fade = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    config: { duration: 2000 }
+  });
+
+  return (
+    <Fragment>
+      <animated.div style={fade}>
+        <p>
+          {props.tagline}
+        </p>
+      </animated.div>
+    </Fragment>
+  )
+}
+
+export default Fade
