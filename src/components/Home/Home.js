342856e0e18e56a46ba0e0be25107130e67fe198
Hayden Phillips <hayden.colony@gmail.com>
2020-09-01 11:01:31 +1200

    Home update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,24 +7,33 @@ import '../../CSS/Home/Home.css'
 import 'antd/dist/antd.css'
 import { CloseOutlined } from '@ant-design/icons'
 
-
 class Cell extends Component {
   render() {
-    const { toggle, name, description, tags, title, css, active, image, background } = this.props
+    const { toggle, name, description, tags, role, roleDes, area, css, active, image, } = this.props
+
     return (
       <div className="cell" style={{ backgroundImage: css, cursor: !active ? 'pointer' : 'auto' }} onClick={!active ? toggle : undefined}>
-        <Fade show={active} delay={active ? 500 : 0}>
+        {/* {active ? <img src={background} className='bg' /> : null} */}
 
-          <div className="full-cell-content" style={{ backgroundImage: background, backgroundSize: 'cover' }}>
+        <Fade show={active} delay={active ? 700 : 0}>
+
+          <div className={'full-cell-content ' + area}>
+            {/* <img className='bg' src={background} /> */}
             <Slug delay={600}>
 
+              <div className='name'>{name}</div>
               <div className="close"><CloseOutlined type="close" style={{ cursor: 'pointer' }} onClick={toggle} /></div>
-              <div className='full-cell-name'>{name}</div>
-              <div className='full-cell-title'>{title}</div>
-              <div className='full-cell-description'>{description}</div>
-              <div className='full-cell-tag'>{tags}</div>
+
+              <div className='description'>{description}</div>
+
+              <div className='role'>{role}</div>
+              <div className='roleDes'>{roleDes}</div>
+
+
+
+              <div className='tag'>{tags}</div>
               <img className='image' src={image} />
-              <div className="circle" style={{ background: css }} />
+              {/* <div className="circle" style={{ background: css }} /> */}
 
             </Slug>
           </div>
@@ -69,6 +78,36 @@ export default class Home extends Component {
           <Cell {...data} active={active} toggle={toggle} />
         )}
       </Grid>
+    )
+  }
+}
+
+
+class roy extends Component {
+  render() {
+    const { toggle, name, tags, image, } = this.props
+
+    return (
+      <div className={'full-cell-roy'}>
+        <Slug delay={600}>
+
+          <div className='name'>{name}</div>
+          <div className="close"><CloseOutlined type="close" style={{ cursor: 'pointer' }} onClick={toggle} /></div>
+
+          <div className='description'>High-end menswear brand based in Wellington New Zealand.</div>
+
+          <div className='role'>Founding Director  Designer</div>
+          <div className='roleDes'>
+            - Designed and manufactured high-quality garments
+            - produced captivating digital content
+            - coordinated all activities required to bring the product to market
+            - manifested sales channels through online and boutique clothing stores across the nation.
+              </div>
+
+          <div className='tag'>{tags}</div>
+          <img className='image' src={image} />
+        </Slug>
+      </div>
     )
   }
 }
