c110ad48680ba2cb4da4bd6dc9df86cc4118ac55
Hayden Phillips <hayden.colony@gmail.com>
2021-05-20 23:29:03 +1200

    fix whitepaper scroll

--- src/components/Projects/Whitepaper.js
+++ src/components/Projects/Whitepaper.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../CSS/Whitepaper/whitepaper.css';
 import whitepaper from '../../assets/images/WHITEPAPER.jpg';
+// import { ReactComponent as PPWhitepaper } from '../../assets/images/WHITEPAPER.jpg';
 
 const Whitepaper = () => {
   return (
     <div className='pdf'>
       <img src={whitepaper} alt='DAO whitepaper'></img>
+      {/* <PPWhitepaper alt='DAO whitepaper'></PPWhitepaper> */}
     </div>
   );
 };
