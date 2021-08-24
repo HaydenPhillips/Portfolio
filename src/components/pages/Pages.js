24e69d6bd1d181f7a3e3c997bc04ffed5f57857b
Hayden Phillips <hayden.colony@gmail.com>
2021-08-24 19:26:25 +1200

    add about page

--- src/components/pages/Pages.js
+++ src/components/pages/Pages.js
@@ -2,89 +2,86 @@ import React from 'react';
 import { Fragment } from 'react';
 import Projects from '../Projects/Projects';
 import LandingPage from '../pages/landingPage/LandingPage';
+import AboutPage from '../pages/aboutPage/AboutPage';
 import ContactPage from '../pages/contactPage/ContactPage';
 import ServicesPage from '../pages/servicesPage/ServicesPage';
-// import {
-//   Asteroid1,
-//   Asteroid2,
-//   Asteroid3,
-// } from '../pages/servicesPage/Asteroid';
 import '../../CSS/Pages/pages.css';
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
 import stars from '../../assets/images/LayeredScene/moon/stars.svg';
 // import { useInView } from 'react-intersection-observer';
 
 const Pages = () => {
-  let parallax;
+	let parallax;
 
-  // const [ref, inView] = useInView({
-  //   threshold: 0,
-  // });
+	// const [ref, inView] = useInView({
+	//   threshold: 0,
+	// });
 
-  return (
-    <Fragment>
-      <Parallax
-        pages={4}
-        ref={(ref) => (parallax = ref)}
-        style={{
-          display: 'grid',
-          margin: 'auto',
-          backgroundRepeat: 'repeat',
-          backgroundColor: '#161b3a',
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        <ParallaxLayer
-          offset={0}
-          speed={-0.2}
-          style={{
-            backgroundImage: `url(${stars})`,
-            backgroundRepeat: 'repeat',
-            backgroundSize: 'cover',
-            width: '100%',
-            height: '120%',
-            opacity: '0.5',
-          }}
-        />
-        <ParallaxLayer
-          offset={0}
-          speed={0.2}
-          style={{
-            backgroundImage: `url(${stars})`,
-            backgroundRepeat: 'repeat',
-            backgroundSize: 'cover',
-            width: '100%',
-            height: '200%',
-          }}
-        />
-        <ParallaxLayer offset={0} speed={1}>
-          <LandingPage></LandingPage>
-        </ParallaxLayer>
+	return (
+		<Fragment>
+			<Parallax
+				pages={4}
+				ref={(ref) => (parallax = ref)}
+				style={{
+					display: 'grid',
+					margin: 'auto',
+					backgroundRepeat: 'repeat',
+					backgroundColor: '#161b3a',
+					width: '100%',
+					height: '100%',
+				}}
+			>
+				<ParallaxLayer
+					offset={0}
+					speed={-0.2}
+					style={{
+						backgroundImage: `url(${stars})`,
+						backgroundRepeat: 'repeat',
+						backgroundSize: 'cover',
+						width: '100%',
+						height: '120%',
+						opacity: '0.5',
+					}}
+				/>
+				<ParallaxLayer
+					offset={0}
+					speed={0.2}
+					style={{
+						backgroundImage: `url(${stars})`,
+						backgroundRepeat: 'repeat',
+						backgroundSize: 'cover',
+						width: '100%',
+						height: '200%',
+					}}
+				/>
+				<ParallaxLayer offset={0} speed={1}>
+					<LandingPage></LandingPage>
+				</ParallaxLayer>
 
-        {/* <div className='in-view' inView={inView}>
+				<ParallaxLayer offset={1} speed={0.5}>
+					<AboutPage></AboutPage>
+				</ParallaxLayer>
+
+				{/* <div className='in-view' inView={inView}>
           <div className='ref' ref={ref}> */}
-        <ParallaxLayer offset={1} speed={0}>
-          <ServicesPage />
-          {/* {inView ? <Asteroid1 /> : console.log(inView)}
-          {inView ? <Asteroid2 /> : console.log(inView)}
-          {inView ? <Asteroid3 /> : console.log(inView)} */}
-        </ParallaxLayer>
-        {/* </div>
+				<ParallaxLayer offset={2.3} speed={0}>
+					<ServicesPage />
+				</ParallaxLayer>
+				{/* </div>
         </div> */}
 
-        <ParallaxLayer offset={2} speed={0.3}>
-          <div className='projects-page'>
-            <Projects></Projects>
-          </div>
-        </ParallaxLayer>
+				<ParallaxLayer offset={3} speed={0.3}>
+					<div className='projects-page'>
+						<Projects></Projects>
+					</div>
+				</ParallaxLayer>
 
-        <ParallaxLayer offset={3} speed={1}>
-          <ContactPage></ContactPage>
-        </ParallaxLayer>
-      </Parallax>
-    </Fragment>
-  );
+				<ParallaxLayer offset={4} speed={1}>
+					<ContactPage></ContactPage>
+				</ParallaxLayer>
+			</Parallax>
+		</Fragment>
+	);
 };
 
 export default Pages;
---
+++ src/components/pages/aboutPage/AboutPage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../../../CSS/Pages/pages.css';
+
+const AboutPage = () => {
+	return (
+		<section className='about-page'>
+			<h1>about</h1>
+			<p>
+				I live in Wellington, New Zealand, and grateful to be nearing my
+				twenty-eighth solar orbit.
+			</p>
+
+			<p>
+				For the past two years I've been working with Vanishing Point Studio as
+				a full-stack dev. Typically my work days consist of building web-apps
+				and wireframes, working with fellow designers and developers, and
+				tending to the todo list one tick at a time.
+			</p>
+
+			<p>
+				I'm passionately curious and open minded, so I often ask a lot of
+				questions. This helps me to be a better problem solver as it gives rise
+				to exploration and discovery of new possibilities.
+			</p>
+
+			<p>
+				My favourite spot is at the intersection of creativity and logic. I am
+				always experimenting and perpetually learning.
+			</p>
+		</section>
+	);
+};
+
+export default AboutPage;
