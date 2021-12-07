519bddad4d36cbf822e3f8933ab18e70c1690269
Hayden Phillips <hayden.colony@gmail.com>
2021-12-08 12:14:21 +1300

    update experience section

--- src/components/pages/experiencePage/ExperiencePage.js
+++ src/components/pages/experiencePage/ExperiencePage.js
@@ -4,130 +4,108 @@ const ExperiencePage = () => {
 	return (
 		<section className='experience-page'>
 			<div className='page-wrap'>
-				<div className='exp-hero'>
+				<div className='experience-block'>
 					<h3 className='title'>
 						web
-						<br /> development
+						<br /> development,
 						<br /> design,
 						<br /> & recreation
 					</h3>
-					<div className='details'>
-						<div className='text-item'>
+					<div className='line' />
+					<ul className='details gap-increase'>
+						<li className='text-item'>
 							<p>
-								For the past three years I have been designing and developing
-								fullstack web applications, through employment and now
-								freelance. I have worked with a range of digital studios,
-								designers, and clients to create outstanding products for the
-								web.
+								I have been designing and developing web applications for the
+								past three years. Working freelance for the most part, with a
+								stint at NZs largest tech shop, ANZ. Throughout this time I have
+								collaborated with a range of clients, digital studios,
+								designers, and developers to create outstanding products for the
+								web. My aim is to deliver beautiful, accessible, and robust
+								screen based interfaces, helping to improve the experience of
+								the user.
 							</p>
-						</div>
-						<div className='text-item'>
+						</li>
+						<li className='text-item'>
 							<p>
-								In my spare time, I mess around playing basketball, table
-								tennis, and chess. I'm always exploring new terrains with a
-								camera in hand, and friends by side. I 'm a curious and open
-								minded individual, so I often ask a lot of questions. This helps
-								me become a better problem solver as it gives rise to
-								exploration and discovery of new possibilities.
+								For fun, I play sports- mainly basketball with a bit of
+								badminton, squash, summer volleyball, and table tennis. I mess
+								around with a few video games like Tekken and GTA, but my
+								favourite game of all is chess. To scratch my creative itch I
+								make clothing, which was my bread and butter prior to my career
+								in software. I'm always exploring new terrains with a camera in
+								hand, and friends by side. I'm a curious and open minded
+								individual, so I often ask a lot of questions. This helps me
+								become a better problem solver as it gives rise to exploration
+								and discovery of new possibilities.
 							</p>
-						</div>
-						<div className='text-item'>
+						</li>
+						<li className='text-item'>
 							<p>
 								The intersection of creativity and logic is the space I wish to
 								play, always experimenting, always learning.
 							</p>
-						</div>
-					</div>
+						</li>
+					</ul>
 				</div>
 
-				<div className='exp-hero'>
+				<div className='experience-block'>
 					<h3 className='title'>
-						software
+						Familiar
+						<br /> software
 						<br /> tools
 					</h3>
-					<div className='details'>
-						<div className='text-item'>
+					<div className='line' />
+					<ul className='details'>
+						<li className='text-item'>
 							<h4 className='subtitle'>React</h4>
-							<p>
-								React is my preferred frontend library for building
-								user-interfaces. I use Hooks in particular as it provides better
-								flexibility in extracting, testing, and reusing code.
-							</p>
-						</div>
-						<div className='text-item'>
+							<p>(hooks)</p>
+						</li>
+						<li className='text-item'>
 							<h4 className='subtitle'>Gatsby</h4>
-							<p>
-								Gatsby is a frontend framework and modern static site generator.
-								I use it for creating fast, secure, and scalable websites.
-							</p>
-						</div>
-						<div className='text-item'>
+						</li>
+						<li className='text-item'>
 							<h4 className='subtitle'>Vue & Nuxt</h4>
-							<p>
-								Vue is a frontend JavaScript framework for building user
-								interfaces and single-page applications. Nuxt offers better SEO
-								improvement with its server-side rendering feature, faster
-								development with auto-generic routing.
-							</p>
-						</div>
-						<div className='text-item'>
+						</li>
+						<li className='text-item'>
 							<h4 className='subtitle'>Strapi</h4>
-							<p>
-								Strapi is an open-source, Node.js based, Headless CMS that saves
-								developers a lot of development time while giving them the
-								freedom to use their favorite tools and frameworks.
-							</p>
-						</div>
-						<div className='text-item'>
-							<h4 className='subtitle'>GraphQL</h4>
-							<p>
-								GraphQL is designed to make APIs fast, flexible, and
-								developer-friendly. As an alternative to REST, GraphQL lets
-								developers construct requests that pull data from multiple data
-								sources in a single API call
-							</p>
-						</div>
-						<div className='text-item'>
+							<p>(headless CMS)</p>
+						</li>
+						<li className='text-item'>
+							<h4 className='subtitle'>Rest APIs & GraphQL</h4>
+						</li>
+						<li className='text-item'>
+							<h4 className='subtitle'>React-Spring & GSAP</h4>
+						</li>
+						<li className='text-item'>
 							<h4 className='subtitle'>JQuery</h4>
-							<p>
-								Working with the DOM directly means it's easy to use and
-								reasonable effective for basic websites.
-							</p>
-						</div>
-					</div>
+						</li>
+						<li className='text-item'>
+							<h4 className='subtitle'>Figma & Xd</h4>
+						</li>
+						<li className='text-item'>
+							<h4 className='subtitle'>Photoshop & Lightroom</h4>
+						</li>
+					</ul>
 				</div>
-				<div className='exp-hero'>
+				<div className='experience-block'>
 					<h3 className='title'>languages</h3>
-					<div className='details'>
-						<div className='text-item'>
-							<h4 className='subtitle'>css</h4>
-							<p>
-								I style components using a pre-processor call scss. It reduces
-								code repetition, and promotes cleaner, more maintainable code.
-							</p>
-						</div>
-						<div className='text-item'>
+					<div className='line' />
+					<ul className='details'>
+						<li className='text-item'>
+							<h4 className='subtitle'>CSS</h4>
+							<p>(SCSS)</p>
+						</li>
+						<li className='text-item'>
 							<h4 className='subtitle'>JavaScript</h4>
-							<p>
-								Most of the code I write is some variation of JavaScript. When
-								its vanilla js, I utilise version ES6 and above.
-							</p>
-						</div>
-						<div className='text-item'>
+							<p>(ES6+)</p>
+						</li>
+						<li className='text-item'>
 							<h4 className='subtitle'>Java</h4>
-							<p>
-								My foundations of programming started with Java. Predominantly
-								use throughout university.
-							</p>
-						</div>
-						<div className='text-item'>
+						</li>
+						<li className='text-item'>
 							<h4 className='subtitle'>Python</h4>
-							<p>
-								Implemented four machine learning programs, a basic data file
-								reader, and small command line games.
-							</p>
-						</div>
-					</div>
+						</li>
+					</ul>
 				</div>
 			</div>
 		</section>
