b4d79e18fbc4ef0490487ba309129590be46f457
Hayden Phillips <hayden.colony@gmail.com>
2021-12-20 11:24:31 +1300

    update and add content

--- src/components/pages/projectsPage/Projects/ProjectZap.js
+++ src/components/pages/projectsPage/Projects/ProjectZap.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react'
-import account_left from '../../../../assets/images/zap_prototype/account-45.svg'
-import account_right from '../../../../assets/images/zap_prototype/account-45-right.svg'
-import zap_logo from '../../../../assets/images/zap_prototype/zap_logo.png'
+import manage_finance_screen from '../../../../assets/images/zap_prototype/manage_finance_screen.svg'
+import mobile_background from '../../../../assets/images/zap_prototype/mobile_background.svg'
+import early_draft from '../../../../assets/images/zap_prototype/early_draft.svg'
 import AnimationIn from './Mobile_prototype_animation'
 import { useInView } from 'react-intersection-observer'
 
 const ProjectZap = () => {
-	const [ref, inView] = useInView({
+	const [prototypeRef, inView] = useInView({
 		threshold: 0,
 		rootMargin: '800px 0px -400px 0px',
 	})
@@ -14,22 +14,21 @@ const ProjectZap = () => {
 	return (
 		<Fragment>
 			<div className='grid-zap'>
-				<div className='grid-c3-even'>
-					<div className='left'>
-						<img src={account_left} alt='account-app' />
+				<div className='content-container'>
+					<div className='col' id='mobile_background'>
+						<img src={mobile_background} alt='account-app' />
 					</div>
-					<div className='mid'>
-						<img src={zap_logo} alt='zap-logo' />
-						<h6>Payments made easy.</h6>
-						<div className='arrow' />
+					<div className='col' id='manage-accounts'>
+						<img src={manage_finance_screen} alt='account-app' />
 					</div>
-					<div className='right'>
-						<img src={account_right} alt='account-app' />
+					<div className='col' id='early-draft'>
+						<img src={early_draft} alt='account-app' />
 					</div>
-				</div>
-
-				<div ref={ref} className='prototype_collection'>
-					{inView ? <AnimationIn /> : <div className='empty'></div>}
+					<div className='col-3'>
+						<div ref={prototypeRef} className='prototype_collection'>
+							{inView ? <AnimationIn /> : <div className='empty'></div>}
+						</div>
+					</div>
 				</div>
 			</div>
 		</Fragment>
