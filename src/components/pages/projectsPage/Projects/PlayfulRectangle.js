3bc8af4f644f9c8492a8d0ace11336a726cba83c
Hayden Phillips <hayden.colony@gmail.com>
2021-12-20 11:22:24 +1300

    reformat code

--- src/components/pages/projectsPage/Projects/PlayfulRectangle.js
+++ src/components/pages/projectsPage/Projects/PlayfulRectangle.js
@@ -1,20 +1,20 @@
-import CardFull from './CardFull';
-import Modal from '../../../Modal';
-import React, { useState, useRef, Fragment } from 'react';
-import { useSpring, animated } from '@react-spring/web';
+import CardFull from './CardFull'
+import Modal from '../../../Modal'
+import React, { useState, useRef, Fragment } from 'react'
+import { useSpring, animated } from '@react-spring/web'
 
 const calc = (x, y, rect) => [
 	-(y - rect.top - rect.height / 2) / 10,
 	(x - rect.left - rect.width / 2) / 10,
 	1.1,
-];
+]
 const trans = (x, y, s) =>
-	`perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+	`perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
-export default function PlayfulCard({ componentName, name, tags, color }) {
-	const ref = useRef(null);
+export default function PlayfulRectangle({ componentName, name, tags, color }) {
+	const ref = useRef(null)
 
-	const [xys, set] = useState([0, 0, 1]);
+	const [xys, set] = useState([0, 0, 1])
 
 	const config = {
 		mass: 1,
@@ -24,13 +24,13 @@ export default function PlayfulCard({ componentName, name, tags, color }) {
 		precision: 0.01,
 		velocity: 0,
 		easing: (t) => t,
-	};
+	}
 
 	const onClose = () => {
-		setIsOpen(false);
-	};
-	const [isOpen, setIsOpen] = useState(false);
-	const props = useSpring({ xys, config });
+		setIsOpen(false)
+	}
+	const [isOpen, setIsOpen] = useState(false)
+	const props = useSpring({ xys, config })
 
 	return (
 		<Fragment>
@@ -41,8 +41,8 @@ export default function PlayfulCard({ componentName, name, tags, color }) {
 						style={{ transform: props.xys.to(trans), backgroundImage: color }}
 						onMouseLeave={() => set([0, 0, 1])}
 						onMouseMove={(e) => {
-							const rect = ref.current.getBoundingClientRect();
-							set(calc(e.clientX, e.clientY, rect));
+							const rect = ref.current.getBoundingClientRect()
+							set(calc(e.clientX, e.clientY, rect))
 						}}
 						onClick={() => setIsOpen(true)}
 					>
@@ -59,5 +59,5 @@ export default function PlayfulCard({ componentName, name, tags, color }) {
 				<CardFull componentName={componentName} onClose={onClose} />
 			</Modal>
 		</Fragment>
-	);
+	)
 }
--- src/components/pages/projectsPage/Projects/Whitepaper.js
+++ src/components/pages/projectsPage/Projects/Whitepaper.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import whitepaper from '../../../../assets/images/WHITEPAPER.jpg';
+import React from 'react'
+import whitepaper from '../../../../assets/images/project_screenshots/planter/WHITEPAPER.jpg'
 
 const Whitepaper = () => {
 	return (
 		<div className='pdf'>
 			<img src={whitepaper} alt='DAO whitepaper' />
 		</div>
-	);
-};
+	)
+}
 
-export default Whitepaper;
+export default Whitepaper
